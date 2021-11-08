84dd6a5cdb4d18a06371f6964d55e48d3c82d21c
yusufluaii <yusuf.luai01@gmail.com>
2021-11-09 00:24:03 +0700

    wordpress init

--- routes-init.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-  var todoList = require('./controller');
-
-  app.route('/').get(todoList.index);
-
-  app.route('/users').get(todoList.users);
-}; 
